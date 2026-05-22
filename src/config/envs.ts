21147292c328146f888f4522118412ce9185ad98
Manu Mdz <manuel.mendozac.dev@gmail.com>
2026-05-22 11:30:41 -0600

    Configuración de NATS

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -3,15 +3,24 @@ import * as joi from 'joi';
 
 interface EnvVars {
   PORT: number;
+  DATABASE_URL: string;
+
+  NATS_SERVERS: string[];
 }
 
 const envsSchema = joi
   .object({
-    PORT: joi.number(),
+    PORT: joi.number().required(),
+    DATABASE_URL: joi.string().required(),
+
+    NATS_SERVERS: joi.array().items(joi.string()).required(),
   })
   .unknown(true);
 
-const res = envsSchema.validate(process.env);
+const res = envsSchema.validate({
+  ...process.env,
+  NATS_SERVERS: process.env.NATS_SERVERS?.split(','),
+});
 
 if (res.error) {
   throw new Error(`Config validation error: ${res.error.message}`);
@@ -21,4 +30,7 @@ const envVars: EnvVars = res.value as EnvVars;
 
 export const envs = {
   port: envVars.PORT,
+  dbUrl: envVars.DATABASE_URL,
+
+  natsServers: envVars.NATS_SERVERS,
 };
--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,16 @@ import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const logger = new Logger('Main');
+
   // const app = await NestFactory.create(AppModule);
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
     {
-      transport: Transport.TCP,
+      // transport: Transport.TCP,
+      transport: Transport.NATS,
       options: {
-        port: envs.port ?? 3000,
+        // port: envs.port ?? 3000,
+        servers: envs.natsServers,
       },
     },
   );
